b5f9eb649aaa15c518b4eeba45bc2f13158a766f
litarobert-am <robert.l@adoreme.com>
2023-07-18 12:29:19 +0300

    fixeds

--- src/test_even.py
+++ src/test_even.py
@@ -3,12 +3,12 @@ from even import is_even
 
 class TestClass:
     def test_one(self):
-        assert isEven(20) == True
-        assert isEven(12312) == True
-        assert isEven(5) == False
-        assert isEven(9) == False
+        assert is_even(20) == True
+        assert is_even(12312) == True
+        assert is_even(5) == False
+        assert is_even(9) == False
 
     def test_two(self):
-        assert isEven(-5) == False
-        assert isEven(-4) == True
-        assert isEven(0) == True
+        assert is_even(-5) == False
+        assert is_even(-4) == True
+        assert is_even(0) == True
